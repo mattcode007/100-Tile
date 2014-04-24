d2bacb3b3b04b50694d1a257970435f9cbc2b975
apple <apple@Apple-2.local>
2014-04-24 21:53:34 +0530

    Initial Commit

---
+++ 100/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  100
+//
+//  Created by apple on 24/04/14.
+//  Copyright (c) 2014 100. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
